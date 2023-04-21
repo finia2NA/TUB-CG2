c95aeec85907c41bccb8ec2d217c349dda7f7546
Your Name <farther-tugboat0o@icloud.com>
2023-04-21 22:52:31 +0200

    add children to pointdatastructures

--- cg2-app/src/model/pointDataStructures.js
+++ cg2-app/src/model/pointDataStructures.js
@@ -1,3 +1,48 @@
+// a class that represents a cube of an octree by specifying two opposite corners
+class octreeCube {
+  constructor(corner1, corner2) {
+    this.corner1 = corner1;
+    this.corner2 = corner2;
+    this.children = []
+  }
+
+  getVolume() {
+    return Math.abs((this.corner1.x - this.corner2.x) * (this.corner1.y - this.corner2.y) * (this.corner1.z - this.corner2.z));
+  }
+}
+
+// a class that represents a plane of a kdtree by specifying a point and an axis.
+// Axis is a vector with all zeros and one one
+class kdTreePlane {
+  constructor(point, axis) {
+    // testsing that axis is valid
+    let count0 = 0;
+    let count1 = 0
+    for (let key in axis) {
+      if (point[key] === 0) {
+        count0++;
+      }
+      if (point[key] === 1) {
+        count1++
+      }
+    }
+    if (!(count0 === axis.length - 1 && count1 === 1)) {
+      throw new Error("invalid kdTree parameters")
+    }
+
+
+    // construct internal vars
+    this.point = point;
+    this.axis = axis;
+
+    this.lesserChildren = []
+    this.higherChildren = []
+
+
+
+  }
+}
+
 class PointDataStructure {
   getAllPoints() { };
   addPoint(point) { };
